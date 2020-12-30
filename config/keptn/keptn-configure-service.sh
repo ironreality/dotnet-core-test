359dee9a39d8e0ca3876ffa7249f6bba5e8805f6
Vadym Surzhyk <ironreality@gmail.com>
2020-12-30 11:51:52 +0200

    Keptn: add SLI/SLO for prod

--- config/keptn/keptn-configure-service.sh
+++ config/keptn/keptn-configure-service.sh
@@ -5,24 +5,35 @@ set -o nounset
 
 proj_name=dotnetcoretest
 
-export GIT_USER=ironreality
-export GIT_REMOTE_URL=https://github.com/ironreality/dotnet-core-test-config
-export GIT_TOKEN=
-
-echo "Creating keptn's project..."
-keptn create project "${proj_name}" --shipyard=./shipyard.yaml --git-user=$GIT_USER --git-token=$GIT_TOKEN --git-remote-url=$GIT_REMOTE_URL
-
 echo "Creating keptn's service..."
 keptn onboard service "${proj_name}" --project="${proj_name}" --chart=../charts/dotnetcoretest
+echo
 
-echo "Adding keptn's test configuration..."
+echo "Adding Jmeter's basic test configuration for qa stage..."
 keptn add-resource --project="${proj_name}" --stage=qa --service="${proj_name}" --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
-keptn add-resource --project="${proj_name}" --stage=staging --service="${proj_name}" --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
+echo
 
-# keptn send event new-artifact --project="${proj_name}" --service="${proj_name}" --image=ironreality/dotnet-core-test --tag=0.0.2
+echo "Adding Jmeter's performance test configuration for staging stage..."
+keptn add-resource --project="${proj_name}" --stage=staging --service="${proj_name}" --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
+echo
 
 # prometheus
+echo "Configure Prometheus monitoring..."
 keptn configure monitoring prometheus --project="${proj_name}" --service="${proj_name}"
+echo
 
+echo "Creating SLI config for stage: staging..."
 keptn add-resource --project="${proj_name}" --stage=staging --service="${proj_name}" --resource=sli-config-prometheus-bg.yaml --resourceUri=prometheus/sli.yaml
+echo
+
+echo "Creating SLO config for stage: staging..."
 keptn add-resource --project="${proj_name}" --stage=staging --service="${proj_name}" --resource=slo-quality-gates.yaml --resourceUri=slo.yaml
+echo
+
+echo "Creating SLI config for stage: production..."
+keptn add-resource --project="${proj_name}" --stage=production --service="${proj_name}" --resource=sli-prod.yaml --resourceUri=prometheus/sli.yaml
+echo
+
+echo "Creating SLI/SLO configs for stage: production..."
+keptn add-resource --project="${proj_name}" --stage=production --service="${proj_name}" --resource=slo-prod.yaml --resourceUri=slo.yaml
+echo
